3acccb9d317cb6065f03fffd87c824a5360c9595
stella <mbithestella0@gmail.com>
2024-05-14 12:37:29 +0300

    made some few changes

--- src/main/java/com/example/myMusicApp/DTOs/ResponseDTO.java
+++ src/main/java/com/example/myMusicApp/DTOs/ResponseDTO.java
@@ -1,14 +1,18 @@
 package com.example.myMusicApp.DTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class ResponseDTO {
     private int statusCode;
     private String statusDescription;
     private List<?> data;
+
+
 }
--- src/main/java/com/example/myMusicApp/utilities/Utilities.java
+++ src/main/java/com/example/myMusicApp/utilities/Utilities.java
@@ -1,4 +1,21 @@
 package com.example.myMusicApp.utilities;
 
+import com.example.myMusicApp.DTOs.ResponseDTO;
+
 public class Utilities {
+    public static ResponseDTO createSuccessfulResponse(String message , Object data){
+        ResponseDTO responseDTO = new ResponseDTO();
+
+//        responseDTO.setStatusCode(200);
+//        responseDTO.setStatusDescription(message);
+//        responseDTO.setData(data instanceof List ? (List<?>) data : List.of(data));
+//        return responseDTO;
+//        }
+//
+//public static ResponseDTO createFailedResponse(Long statusCode , String message){
+//    ResponseDTO responseDTO = new ResponseDTO();
+//    responseDTO.setStatusCode(statusCode);
+//    responseDTO.setStatusDescription(message);
+//    return responseDTO;
+//}
 }
