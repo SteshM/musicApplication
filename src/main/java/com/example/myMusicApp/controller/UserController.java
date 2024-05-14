4b8d0f29cd38726846b94056e06b9fdb62e322e6
stella <mbithestella0@gmail.com>
2024-05-14 09:18:14 +0300

    created te user entity

--- src/main/java/com/example/myMusicApp/controller/UserController.java
+++ src/main/java/com/example/myMusicApp/controller/UserController.java
@@ -3,11 +3,15 @@ package com.example.myMusicApp.controller;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Data
+@Slf4j
+@RestController
 @RequiredArgsConstructor
 @NoArgsConstructor
-
-
+@RequestMapping("/v1")
 public class UserController {
+
 }
--- src/main/java/com/example/myMusicApp/entities/UserEntity.java
+++ src/main/java/com/example/myMusicApp/entities/UserEntity.java
@@ -1,4 +1,19 @@
 package com.example.myMusicApp.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    private String name;
+    private String email;
 }
