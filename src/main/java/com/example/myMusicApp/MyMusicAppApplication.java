9259ba98904a58d01d9e22b35bc5ab8acb390068
stella <mbithestella0@gmail.com>
2024-05-14 08:11:07 +0300

    Initial commit

---
+++ src/main/java/com/example/myMusicApp/MyMusicAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myMusicApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMusicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMusicAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myMusicApp/MyMusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myMusicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMusicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
