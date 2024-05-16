1c44767df186209fa15d69ae20406d3174593c0b
stella <mbithestella0@gmail.com>
2024-05-16 15:34:31 +0300

    created playlist entity

--- src/main/java/com/example/myMusicApp/entities/PlaylistEntity.java
+++ src/main/java/com/example/myMusicApp/entities/PlaylistEntity.java
@@ -1,4 +1,14 @@
 package com.example.myMusicApp.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "playlists")
 public class PlaylistEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int playlistId;
+    private String playlistName;
 }
